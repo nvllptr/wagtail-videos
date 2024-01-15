dceac4a8b3c0b7862ca28258db0bd546e3745aa0
Nikita Phung <nikitaphung2000@gmail.com>
2024-01-15 16:18:26 +1100

    Updated video chooser widget
    Requests video data for thumbnail and title
    Displays thumbnail and title for video chooser widget

--- wagtailvideos/static/wagtailvideos/js/video-chooser.js
+++ wagtailvideos/static/wagtailvideos/js/video-chooser.js
@@ -1,9 +1,25 @@
 function createVideoChooser(id) {
     var chooserElement = $('#' + id + '-chooser');
     var previewVideo = chooserElement.find('.preview-image img');
+    var previewVideoTitle = chooserElement.find('.video-title');
+
     var input = $('#' + id);
     var editLink = chooserElement.find('.edit-link');
-
+    document.addEventListener('DOMContentLoaded', event => {
+        var input = $('#' + id);
+        let call_url = window.chooserUrls.videoChooser + input.val();
+        $.ajax(call_url).done(function(data) {
+            let videoData = data.result
+            input.val(videoData.id);
+            previewVideo.attr({
+                src: videoData.preview.url,
+                alt: videoData.title
+            });
+            previewVideoTitle.text(videoData.title)
+            chooserElement.removeClass('blank');
+            editLink.attr('href', videoData.edit_link);
+        });
+    })
     $('.action-choose', chooserElement).click(function() {
         ModalWorkflow({
             url: window.chooserUrls.videoChooser,
